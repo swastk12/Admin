6895a3a69d76f36426d203952643661a240372e5
unknown <swastikdubey1234@gmail.com>
2022-11-27 04:47:22 +0530

    ch

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const categoryRoute = require("./routes/categorys");
 const path = require("path");
 const multer = require("multer");
 
-const PORT = process.env.PORT || 3300;
+const PORT = process.env.PORT || 3500;
 
 dotenv.config();
 app.use(express.json());
